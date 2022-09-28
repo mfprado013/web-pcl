de0b6832be9e0c8b485ed3b15e485cd5e05b679a
Abner André <abner@oialbert.com.br>
2022-09-28 18:39:06 -0300

    feat: craido nova home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'cinza-900': '#171716',
+        'cinza-800': '#212121',
+        'cinza-700': '#363636',
+        'cinza-500': '#474747',
+        'cinza-400': '#6b6b6b',
+        'cinza-300': '#9e9e9e',
+        'cinza-200': '#bab8b8',
+        'cinza-100': '#d6d6d6',
+        'cinza-50': '#f5f5f5'
+      }
+    },
   },
   plugins: [],
 }
