886fc9ed4780b41bfc9ed77266f2a5322e545b5e
Abner André <abner@oialbert.com.br>
2022-09-30 14:43:41 -0300

    feat: criado about da home e slide de empresas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { SiteLayout } from '@/components/Layouts';
 import CarouselSlide from '@/components/Pages/Home/Carousel/CarouselSlide';
 
+import malePicture from '@/assets/images/male-worker.png';
+import Link from 'next/link';
+import CompaniesSlides from '@/components/Pages/Home/Carousel/CompaniesSlides';
+
 const navigation = [
   { name: 'Inicio', href: '/', current: true },
   { name: 'Sobre', href: '/about', current: false },
@@ -38,10 +42,34 @@ export default function Index() {
             alt=''
           /> */}
         </header>
-        <div className='max-w-7xl mx-auto'>
-          <main>
-            <h1>ponto certo</h1>
-          </main>
+        {/* about */}
+        <div className='max-w-7xl mx-auto mt-10'>
+          <div className='flex justify-between mb-10'>
+            <div className='pl-10 pt-10 flex flex-col'>
+              <h2 className='break-words font-semibold text-4xl text-cinza-600'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+                Praesent libero. Sed cursus ante{' '}
+                <span className='text-blue-600'>dapibus diam.</span> Sed nisi. Nulla quis
+                sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
+              </h2>
+              <div className='mt-6 mr-5 sm:mr-0'>
+                <Link href='/portifolio'>
+                  <a className='bg-blue-600 hover:bg-blue-500 text-gray-50 uppercase font-semibold text-center w-full sm:w-52 px-5 py-3 flex justify-center'>
+                    saiba mais
+                  </a>
+                </Link>
+              </div>
+            </div>
+            <img src={malePicture.src} alt='about-bussiness' className='hidden md:flex' />
+          </div>
+        </div>
+        {/*  */}
+        <div className='bg-cinza-800 w-full h-52 mb-10'></div>
+        {/* companies slides */}
+        <div className=' max-w-xs sm:max-w-lg md:max-w-3xl  lg:max-w-5xl xl:max-w-7xl mx-auto mt-10'>
+          <div className=''>
+            <CompaniesSlides />
+          </div>
         </div>
         {/* <div className='mx-auto max-w-7xl bg-white'></div> */}
       </SiteLayout>
